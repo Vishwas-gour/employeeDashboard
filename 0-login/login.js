ea0a9930860930f7ecc321463f6e463ca25e27e4
Vishwas-gour <vishwasgour2002@gmail.com>
2024-12-06 00:25:56 +0530

    add some more functionality in login/registration page

--- 0-login/login.js
+++ 0-login/login.js
@@ -1,9 +1,51 @@
 // localStorage.clear();
-let btn = document.getElementById("btn");
-console.log(btn);
-btn.addEventListener("click", login);
+let create = document.querySelector("#btn");
+let sign = document.querySelector("#sign-up");
+let forget = document.querySelector(".forget-password");
+let submit = document.querySelector("#submit-btn");
+let generate = document.querySelector("#generate-btn");
+let otpInput = document.querySelector("#otp");
+let emailF = document.querySelector("#emailF");
+console.log(create);
+console.log(sign);
+create.addEventListener("click", login);
+sign.addEventListener("click", (e) => {
+    window.location.href = "registration.html";
+});
+// <<<<<<<<<--OTP-Forget-->>>>>>>>>>>>>>
+forget.addEventListener("click", () => {
+    console.log("i am in ");
+    
+    generate.addEventListener("click", (e) => {
+        generate.style.display = "none";
+        otpInput.style.display = "inline";
+        submit.style.width = "40%";
+        submit.style.display = "block";
+
+        let otp = "";
+        for (let i = 0; i < 4; i++) {
+            otp += Math.trunc(Math.random() * 4);
+        }
+        localStorage.setItem("forgetOtp", otp);
+        console.log(otp);
+            // <<<<<<<<<--submit-Forget-OTP->>>>>>>>>>>>>>
+
+        submit.addEventListener("click", (e) => {
+            console.log(otpInput.value);
+            console.log(localStorage.forget-otp)
+            if (otpInput.value != localStorage.forgetOtp) {
+                alert("Wrong OTP try again");
+                return;
+            }
+            alert(`Your password => ${localStorage.password}`);
+    
+        })
+
+    })
+})
 
 function login(e) {
+    console.log(e.target)
     e.preventDefault();
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -16,9 +58,9 @@ function login(e) {
     }
     else if (email != localStorage.email || password != localStorage.password) {
         e.preventDefault();
-        console.log(localStorage.email,localStorage.password)
-        let result = confirm("user have't account you want to create");
-        if (result)window.location.href = "registration.html";
+        console.log(localStorage.email, localStorage.password)
+        let result = confirm("You haven't account, do you want to create");
+        if (result) window.location.href = "registration.html";
         else return;
     }
     else {
--- 0-login/registration.js
+++ 0-login/registration.js
@@ -1,32 +1,51 @@
 // localStorage.clear();
-document.getElementById("btn").addEventListener("click", add);
-
-function add(e) {
+let submit = document.querySelector("#submit-btn");
+let generate = document.querySelector("#generate-btn");
+let otpInput = document.querySelector("#otp");
+console.log(submit, generate, otpInput)
+generate.addEventListener("click", async (e) => {
     e.preventDefault();
-    let username = document.getElementById("username").value;
+
+    generate.style.display = "none";
+    otpInput.style.display = "inline";
+    submit.style.width = "40%";
+    submit.style.display = "block";
+
+    let otp = "";
+    for (let i = 0; i < 4; i++) {
+        otp += Math.trunc(Math.random() * 4);
+    }
+    localStorage.setItem("otp", otp);
+    console.log(otp);
+
+    // let pr = prompt("asdf");
+
     let email = document.getElementById("email").value;
+    let firstName = document.getElementById("first-name").value;
+    let lastName = document.getElementById("last-name").value;
     let password = document.getElementById("password").value;
-    let cnfpsw = document.getElementById("psw").value;
-    if (username == "" || email == "" || password == "" || cnfpsw == "") {
+    let cnfpsw = document.getElementById("confirm-password").value;
+    if (firstName == "" || email == "" || password == "" || cnfpsw == "") {
         alert("all fieild are mandetry");
         return;
     } else if (password !== cnfpsw) {
         alert("Password not match");
         return
     }
-    localStorage.setItem("username", username);
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
     localStorage.setItem("email", email);
     localStorage.setItem("password", password);
 
-    window.location.href = "login.html";
+    submit.addEventListener("click", (e) => {
+        console.log(otpInput.value);
+        if (otpInput.value != localStorage.otp) {
+            alert("Wrong OTP try again");
+            return;
+        }
+        alert("registration successfull");
 
-    alert("registration successfull");
+    })
 
-}
-
-
-let verify = document.getElementsByClassName("otp-button");
-console.log(ver)
-verify.addEventListener('click', function() {
- alert('Element clicked!');
 });
+
