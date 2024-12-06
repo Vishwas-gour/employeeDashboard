217dfa584bca4b4e77074f7fe5eebc829270506b
Vishwas-gour <vishwasgour2002@gmail.com>
2024-12-06 15:40:54 +0530

    improve responsiveness about-corporation > countainer

--- 0-login/login.js
+++ 0-login/login.js
@@ -1,50 +1,63 @@
 // localStorage.clear();
-let create = document.querySelector("#btn");
+let create = document.querySelector("#login");
 let sign = document.querySelector("#sign-up");
 let forgetPassword = document.querySelector(".forget-password");
-let submit = document.querySelector("#submit-btn");
+let submitOTP = document.querySelector("#submit-btn");
 let generate = document.querySelector("#generate-btn");
 let otpInput = document.querySelector("#otp");
-let emailF = document.querySelector("#emailF");
+let forgetEmail = document.querySelector("#emailF");
 let login = document.querySelector("#login");
-let forget = document.querySelector("#forget");
-console.log(create);
-console.log(sign);
+let firstName = document.querySelector("#first-name");
+let lastName = document.querySelector("#last-name");
+let forgetPage = document.querySelector("#forget_page");
+let loginPage = document.querySelector("#login_page");
+console.log(otpInput)
+
 sign.addEventListener("click", (e) => {
     window.location.href = "registration.html";
 });
+
 // <<<<<<<<<--OTP-Forget-->>>>>>>>>>>>>>
 forgetPassword.addEventListener("click", () => {
-    forget.style.margin ="-34vw" ////// ***********CONTINUE ON 6 DEC****************
     console.log("i am in ");
+    loginPage.style.zIndex = "0";
+    loginPage.style.opacity = "0";
+
+    forgetPage.style.zIndex = "1";
+    forgetPage.style.opacity = "1";
+
     generate.addEventListener("click", (e) => {
-        if (emailF != localStorage.email) {
-            let Confirm = confirm("You don't have Account, do you want to craeat");
+        
+        if (forgetEmail.value != localStorage.email) {
+            let Confirm = confirm("You don't have Account, do you want to create");
+            window.location.href = "registration.html"
         }
+        
         generate.style.display = "none";
         otpInput.style.display = "inline";
-        submit.style.width = "40%";
-        submit.style.display = "block";
+        submitOTP.style.width = "40%";
+        submitOTP.style.display = "block";
 
         let otp = "";
         for (let i = 0; i < 4; i++) {
             otp += Math.trunc(Math.random() * 4);
         }
         localStorage.setItem("forgetOtp", otp);
-        console.log(otp);
+        console.log(`Do not share OTP to anyone: %c${otp}`, 'color: orange');
         // <<<<<<<<<--submit-Forget-OTP->>>>>>>>>>>>>>
 
-        submit.addEventListener("click", (e) => {
-            console.log(otpInput.value);
-            console.log(localStorage.forget - otp)
+        submitOTP.addEventListener("click", (e) => {
             if (otpInput.value != localStorage.forgetOtp) {
                 alert("Wrong OTP try again");
                 return;
             }
-            alert(`Your password => ${localStorage.password}`);
+            alert(`Your password  is(${localStorage.password})`);
+            loginPage.style.zIndex = "1";
+            loginPage.style.opacity = "1";
 
+            forgetPage.style.zIndex = "0";
+            forgetPage.style.opacity = "0";
         })
-
     })
 })
 // =====================================================
@@ -53,20 +66,26 @@ create.addEventListener("click", (e) => {
         console.log(e.target)
         e.preventDefault();
         let email = document.getElementById("email").value;
+        console.log("value", email)
         let password = document.getElementById("password").value;
         let form = document.getElementById("form");
         console.log(form, email, password)
         if (email == "" || password == "") {
             e.preventDefault();
-            alert("all fieild are mandetry");
+            alert(" fieild are mandetry");
             return;
         }
-        else if (email != localStorage.email || password != localStorage.password) {
+        else if (email != localStorage.email && password != localStorage.password) {
             e.preventDefault();
+            console.log("local value", localStorage.email)
             console.log(localStorage.email, localStorage.password)
-            let result = confirm("You haven't account, do you want to create");
+            let result = confirm("You don't have an account, do you want to create");
             if (result) window.location.href = "registration.html";
             else return;
+        }
+        else if (email == localStorage.email && password != localStorage.password) { 
+           alert("Wrong password");
+           return;
         }
         else {
             alert("congratulation you are succesfuly log in ")
--- 0-login/registration.js
+++ 0-login/registration.js
@@ -6,46 +6,56 @@ console.log(submit, generate, otpInput)
 generate.addEventListener("click", async (e) => {
     e.preventDefault();
 
-    generate.style.display = "none";
-    otpInput.style.display = "inline";
-    submit.style.width = "40%";
-    submit.style.display = "block";
-
-    let otp = "";
-    for (let i = 0; i < 4; i++) {
-        otp += Math.trunc(Math.random() * 4);
-    }
-    localStorage.setItem("otp", otp);
-    console.log(otp);
-
-    // let pr = prompt("asdf");
-
+    // ---> take the data of all input
     let email = document.getElementById("email").value;
     let firstName = document.getElementById("first-name").value;
     let lastName = document.getElementById("last-name").value;
     let password = document.getElementById("password").value;
     let cnfpsw = document.getElementById("confirm-password").value;
+    // ---> if inputs are empty
     if (firstName == "" || email == "" || password == "" || cnfpsw == "") {
-        alert("all fieild are mandetry");
-        return;
-    } else if (password !== cnfpsw) {
-        alert("Password not match");
-        return
+        alert("all fieild are mandotry");
     }
-    localStorage.setItem("firstName", firstName);
-    localStorage.setItem("lastName", lastName);
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
-
-    submit.addEventListener("click", (e) => {
-        console.log(otpInput.value);
-        if (otpInput.value != localStorage.otp) {
-            alert("Wrong OTP try again");
-            return;
+    else if (password !== cnfpsw) {
+        // ---> if password && confirm-password not don't match
+        alert("Password not match");
+        return;
+    }
+    else {
+        //  <<<<<<<<<<-IF EVERY-THING-IS-FINE ->>>>>>>>>>>
+        // ---> generating otp
+        let otp = "";
+        for (let i = 0; i < 4; i++) {
+            otp += Math.trunc(Math.random() * 10);
         }
-        alert("registration successfull");
+        localStorage.setItem("otp", otp);
+        // styling on buttons  
+        generate.style.display = "none";
+        otpInput.style.display = "inline";
+        submit.style.width = "40%";
+        submit.style.display = "block";
 
-    })
+        console.log(`Do not share OTP to anyone: %c${otp}`, 'color: orange');
+        alert("check your Gmail")
+        // ---> while registration store all data in localStorage
+        localStorage.setItem("firstName", firstName);
+        localStorage.setItem("lastName", lastName);
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
 
+        submit.addEventListener("click", (e) => {
+            e.preventDefault();
+            // ---> in the case of OTP not match 
+            if (otpInput.value != localStorage.otp) {
+                alert("Wrong OTP try again");
+            } else {
+                // ---> in the case of OTP match render to loginpage 
+
+                alert("New Account has created");
+                window.location.href = "login.html"
+            }
+
+        })
+    }
 });
 
